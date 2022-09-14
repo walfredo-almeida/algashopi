e273f630eee470bca1341fb25e7904b25c77fa64
Victor Souza <mesouzavictor@gmail.com>
2022-09-14 15:19:26 -0300

    add base layout

--- src/components/AppContainer/AppContainer.styles.js
+++ src/components/AppContainer/AppContainer.styles.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Wrapper = styled.main`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-column-gap: 0ch;
+    grid-column-gap: 24px;
 
     padding: 24px;
 
