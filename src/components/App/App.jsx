5a41d31d73063d8192c76fc3ceb846e5eee3dd6e
walfredo <awalfredo@hotmail.com>
2022-09-16 09:05:20 -0300

    user change

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppContainer from '../AppContainer'
 import AppHeader from '../AppHeader'
 import { Container, Wrapper } from './App.styles'
-
+import Checkbox from '../../shared/Checkbox'
 
 function App() {
+    const [lettuce, ] = useState(true)
+
     return <Wrapper>
         <Container>
         <AppHeader/>
         <AppContainer
-        left ={<div style={{ backgroundColor: 'red'}}> produtos disponiveis </div>}       
-        middle ={<div style={{ backgroundColor: 'green'}}> sua lista de compras </div>}       
-        right ={<div style={{ backgroundColor: 'blue'}}> estatisticas</div>}       
+        left ={<div>  produtos disponiveis
+            <Checkbox 
+            value = {lettuce} 
+            title="alface" 
+            onClick={() => window.alert('batata')}/>
+            <Checkbox 
+            value = {false} 
+            title="arroz" />
+            </div>}  
+        middle ={<div> sua lista de compras </div>}       
+        right ={<div> estatisticas</div>}       
         
         />
 
